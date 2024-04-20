41d31d27f963cd439c628fb4e336fbfb07b6e093
viqueen <hasnae@viqueen.org>
2024-04-20 15:26:16 +1000

    generate simple code from todo schema

---
+++ _api/node-sdk/src/index.ts
@@ -0,0 +1,2 @@
+export * as TodoV1Models from './todo/v1/models';
+export * as TodoV1Service from './todo/v1/todo_service';
---
+++ _api/web-sdk/src/index.ts
@@ -0,0 +1,3 @@
+export * as TodoV1Models from './todo/v1/models_pb';
+export * as TodoV1Service from './todo/v1/todo_service_pb';
+export * as TodoV1Connect from './todo/v1/todo_service_connect';
