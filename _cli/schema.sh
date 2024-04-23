0a2f73641f8a2c4400942c5d6e6c686bc769a050
viqueen <hasnae.rehioui@safetyculture.io>
2024-04-23 21:00:33 +1000

    more hax

--- _cli/schema.sh
+++ _cli/schema.sh
@@ -4,15 +4,16 @@ set -ex
 
 function tools() {
   cd _schema/protobuf-gen || exit
-  docker build -t monorepo-template-protobuf-gen .
+  docker build -t viqueen/monorepo-template-protobuf-gen .
 }
 
 function codegen() {
-  registry=${1}
+  registry=${1:-""}
+  version=${2:-""}
   docker run \
     --volume "${PWD}/_schema:/workspace/schema" \
     --volume "${PWD}/_api:/workspace/api" \
-    "${registry}"monorepo-template-protobuf-gen buf generate --verbose
+    "${registry}"viqueen/monorepo-template-protobuf-gen"${version}" buf generate --verbose
 }
 
 function lint() {
